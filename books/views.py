1fdf4fb851d802a940bb62b5b5989c7252f38843
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-04-17 23:25:20 +0000

    added detail view to htmx and stuff

--- books/views.py
+++ books/views.py
@@ -8,18 +8,24 @@ from .forms import BookFormSet,BookForm
 def create_book(request,pk):
     author = Author.objects.get(pk=pk)
     # pass the request.POST or none
-    formset = BookFormSet(request.POST or None)
+    form = BookForm(request.POST or None)
+    books = Book.objects.filter(author=author)
 
     if request.method == 'POST':
-        if formset.is_valid():
+        if form.is_valid():
+            book = form.save(commit=False)
             # assigning the instance thus the item of info typical to the class formset to author
-            formset.instance = author
-            formset.save()
-            return redirect("create-book",pk=author.id)
+            book.author = author
+            book.save()
+            return redirect("detail-book",pk=book.id)
+        else:
+            # this should run when there are errors in the form the form should be resubmitted
+            return render(request,"partials/bookform.html",{"form":form})
 
     context = {
-        "formset":formset,
-        "author":author
+        "form":form,
+        "author":author,
+        "books":books
     }
     return render(request,"create_book.html",context)
 
@@ -30,3 +36,11 @@ def create_book_form(request):
         "form": form
     }
     return render(request,'partials/bookform.html',context)
+
+
+def detail_book(request,pk):
+    book = Book.objects.get(pk=pk)
+    context = {
+        "book": book
+    }
+    return render(request,'partials/book_detail.html',context)
